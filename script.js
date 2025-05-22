5e7f3a1da967e5e7c115ee3ede6b7ded0d2d5f20
HackerEdition <nicatmmmdov452@gmail.com>
2025-05-22 14:57:27 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Spinner bitəndə əsas hissəni göstər
+window.addEventListener("load", function () {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("main-content").style.display = "block";
+});
