5cdc94e187a33e4ece97f2d208fb72d892c72571
HackerEdition <nicatmmmdov452@gmail.com>
2025-05-23 13:03:08 +0400

    Update update_user_data.php

--- update_user_data.php
+++ update_user_data.php
@@ -1,16 +1,31 @@
 <?php
-$data = json_decode(file_get_contents("php://input"), true);
+header('Content-Type: application/json');
 
-$conn = new mysqli("sql8.freesqldatabase.com", "sql8780594", "s3Alaib8pR", "sql8780594");
-if ($conn->connect_error) die("Bağlantı xətası");
+$host = 'sql8.freesqldatabase.com';
+$db = 'sql8780594';
+$user = 'sql8780594';
+$pass = 's3Alaib8pR';
+
+$conn = new mysqli($host, $user, $pass, $db, 3306);
+
+if ($conn->connect_error) {
+    echo json_encode(['error' => 'MySQL bağlantı xətası: ' . $conn->connect_error]);
+    exit();
+}
+
+$data = json_decode(file_get_contents('php://input'), true);
 
 $id = intval($data['id']);
-$views = intval($data['ad_views']);
-$coins = intval($data['coin_balance']);
+$ad_views = intval($data['ad_views']);
+$coin_balance = intval($data['coin_balance']);
 
-// Mövcud deyilsə, əlavə et
-$conn->query("INSERT IGNORE INTO users (id, ad_views, coin_balance) VALUES ($id, 0, 0)");
+$sql = "UPDATE users SET ad_views = $ad_views, coin_balance = $coin_balance WHERE id = $id";
 
-$sql = "UPDATE users SET ad_views = $views, coin_balance = $coins WHERE id = $id";
-$conn->query($sql);
+if ($conn->query($sql)) {
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['error' => 'Yenilənmə xətası']);
+}
+
+$conn->close();
 ?>
